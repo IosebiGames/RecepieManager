2f88a8661d2f41d10459ed145aa2c9fc184cb104
IosebiGames <127665740+IosebiGames@users.noreply.github.com>
2025-12-23 18:21:20 +0400

    Sound.java

--- RecipieManager/sound/Sound.java
+++ RecipieManager/sound/Sound.java
@@ -1,22 +1,17 @@
 package sound;
 
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-
+import javax.sound.sampled.*;
 import Tools.ResourceLoader;
 
 public class Sound {
-	
 	private ResourceLoader rl;
-	private String sound;
 	private Clip clip;
 	
 	public Sound() {
 		  rl = new ResourceLoader();
-		  sound = rl.getSoundFile("/sound/click_sound.wav");
 		  try {
            clip = AudioSystem.getClip();
-       	   clip.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream(sound)));
+       	   clip.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream(rl.getSoundFile("/sound/click_sound.wav"))));
           }catch(Exception e) {
         	 IO.println("Failed to prepare the sound: " + e.getMessage());
         }
